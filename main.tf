2354f83f4a8423871968f76c303bd83d0ab8ca8c
Roman Schwarz <rs@cloudeteer.de>
2024-06-18 14:06:01 +0200

    Add azurerm provider

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {
 }
